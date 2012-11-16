72d074d8931a87a14b8510607a588ca03e1e65f7
jcvasquez2012 <jcva18@hotmail.com>
2012-11-16 01:25:19 -0400

    commit rama 1

--- aplicationJC/Form1.cs
+++ aplicationJC/Form1.cs
@@ -19,6 +19,8 @@ namespace aplicationJC
         private void button1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("hola mundo");
+            int a = 4;
+
         }
     }
 }
