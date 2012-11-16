21b720427c63ef1774794b4ee7c8faacd0ef759b
jcvasquez2012 <jcva18@hotmail.com>
2012-11-16 01:10:21 -0400

    commit jc

--- aplicationJC/Form1.cs
+++ aplicationJC/Form1.cs
@@ -15,5 +15,10 @@ namespace aplicationJC
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("hola mundo");
+        }
     }
 }
